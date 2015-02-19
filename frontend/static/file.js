3df74af271012f9743b95045aa4c23c96073c83e
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-18 22:25:45 -0800

    jslint

--- frontend/static/file.js
+++ frontend/static/file.js
@@ -1,234 +1,242 @@
-var RENDERED_PLAIN_COLORIZED;
-var RENDERED_BIBTEX;
-var API_DATA;
+/*jslint browser: true*/
+/*global $, jQuery, alert, _*/
+"use strict";
 
-var query = function (q) {
-    $.get('/resolve', {'q': q})
-        .done(function(data) {
-            API_DATA = data;
+(function () {
 
-            // rest these. they'll need to be recomputed.
-            RENDERED_PLAIN_COLORIZED = null;
-            RENDERED_BIBTEX = null;
-            render_output_zone();
-    });
-};
+    var RENDERED_PLAIN_COLORIZED,
+        RENDERED_BIBTEX,
+        API_DATA;
 
+    /*
+        Hit the server API.
+    */
+    function query(q) {
+        $.get('/resolve', {'q': q})
+            .done(function (data) {
+                API_DATA = data;
 
-var render_output_zone = function () {
-    var fmt = $('#btn-toggle-format').text().trim()
-    var outputzone = $('#output-zone')
-
-    if (fmt == 'BibTeX') {
-        if (RENDERED_BIBTEX == null)
-            RENDERED_BIBTEX = build_bibtex(API_DATA);
-        $('#output-zone').text(RENDERED_BIBTEX);
-    } else {
-        if (RENDERED_PLAIN_COLORIZED == null)
-            RENDERED_PLAIN_COLORIZED = build_plain_colorized(API_DATA);
-        outputzone.empty();
-        outputzone.append(RENDERED_PLAIN_COLORIZED);
-    }
-};
-
-
-var build_bibtex = function (data) {
-    var zipper = [];
-
-    // Filter out 'None' tags. Form zipper
-    // which is list(zip(tokens, tags))
-    for (var i = 0; i < data['tokens'].length; i++) {
-        var tok = data['tokens'][i];
-        var tag = data['tags'][i];
-        if (tag == 'None') {
-            continue;
-        }
-        zipper.push([tag, tok]);
+                // reset these. they'll need to be recomputed.
+                RENDERED_PLAIN_COLORIZED = null;
+                RENDERED_BIBTEX = null;
+                render_output_zone();
+            });
     }
 
+    /*
+        Compute whatever represntation is supposed to be shown
+        in the output box if necessary, and display it.
+    */
+    function render_output_zone() {
+        var fmt = $('#btn-toggle-format').text().trim(),
+            outputzone = $('#output-zone');
 
-    // Chunk the zipper into contiguous runs of the same tag.
-    var last_tag = null;
-    var chunk_toks = [];   // final length n_runs.
-    var chunk_tags = [];   // final length n_runs.
-    // each chunk_toks[i] is a list of variable length, with the tokens
-    // in that run of consecutive tag.
-    for (var i = 0; i < zipper.length; i++) {
-        var tag = zipper[i][0];
-        var tok = zipper[i][1];
-        if (tag == last_tag) {
-            chunk_toks[chunk_toks.length-1].push(tok);
+        if (fmt === 'BibTeX') {
+            if (RENDERED_BIBTEX === null) {
+                RENDERED_BIBTEX = build_bibtex(API_DATA);
+            }
+            $('#output-zone').text(RENDERED_BIBTEX);
         } else {
-            chunk_toks.push([tok])
-            chunk_tags.push(tag);
-        }
-
-        last_tag = tag;
-    }
-
-    fields = {
-        authors: [{}],
-        title: [],
-        journal: [],
-        volume: [],
-        year: [],
-        issue: [],
-        page: [],
-    }
-    for (var i = 0; i < chunk_toks.length; i++) {
-        if (chunk_tags[i] == 'journ') {
-            Array.prototype.push.apply(fields['journal'], chunk_toks[i]);
-        } else if (chunk_tags[i] == 'title') {
-            Array.prototype.push.apply(fields['title'], chunk_toks[i]);
-        } else if (chunk_tags[i] == 'vol') {
-            Array.prototype.push.apply(fields['volume'], chunk_toks[i]);
-        } else if (chunk_tags[i] == 'year') {
-            Array.prototype.push.apply(fields['year'], chunk_toks[i]);
-        }  else if (chunk_tags[i] == 'page') {
-            Array.prototype.push.apply(fields['page'], chunk_toks[i]);
-        } else if (chunk_tags[i] == 'issue') {
-            Array.prototype.push.apply(fields['issue'], chunk_toks[i]);
-        } else if (chunk_tags[i] == 'given' || chunk_tags[i] == 'fam') {
-            var authors = fields['authors'];
-            if (Object.keys(authors[authors.length-1]).length == 2) {
-                authors.push({});
+            if (RENDERED_PLAIN_COLORIZED === null) {
+                RENDERED_PLAIN_COLORIZED = build_plain_colorized(API_DATA);
             }
-            var last_auth = authors[authors.length-1];
-            last_auth[chunk_tags[i]] = chunk_toks[i];
+            outputzone.empty();
+            outputzone.append(RENDERED_PLAIN_COLORIZED);
         }
     }
 
-    // format the author list
-    var author_records = fields['authors'];
-    var authors = [];
-    for (var i = 0; i < author_records.length; i++) {
-        var g = author_records[i]['given'];
-        if (g === undefined)
-            g = [];
-        var f = author_records[i]['fam'];
-        if (f === undefined)
-            f = [];
-        authors.push(join_tokens(g.concat(f)));
-    }
 
-    var author_list = authors.join(' and ');
-    var citekey = 'citekey'
-    if (author_records.length > 0) {
-        citekey = author_records[0]['fam'] + join_tokens(fields['year'])
-        if (fields['title'].length > 0)
-            citekey += fields['title'][0]
-    }
+    /*
+        Build the (colored) plain text representation of the tokenized/tagged
+        data returned from the API.
+    */
+    function build_plain_colorized(data) {
+        var i,
+            o = $('<div></div>'),
+            n = data['tokens'].length,
+            tok,
+            tag;
 
-    var lines = ['@author{' + citekey];
-    if (author_list.length > 0)
-        lines.push('  author = {' + author_list + '}');
-    if (fields['title'].length > 0)
-        lines.push('  title = {' + join_tokens(fields['title']) + '}');
-    if (fields['journal'].length > 0)
-        lines.push('  journal = {' + join_tokens(fields['journal']) + '}');
-    if (fields['year'].length > 0)
-        lines.push('  year = {' + join_tokens(fields['year']) + '}');
-    if (fields['volume'].length > 0)
-        lines.push('  volume = {' + join_tokens(fields['volume']) + '}');
-    if (fields['issue'].length > 0)
-        lines.push('  issue= {' + join_tokens(fields['issue']) + '}');
-    if (fields['page'].length > 0)
-        lines.push('  page= {' + join_tokens(fields['page']) + '}');
+        o.text('');
+        for (i = 0; i < n; i += 1) {
+            tok = data.tokens[i];
+            tag = data.tags[i];
+            o.append($('<span></span>').text(tok).addClass(tag));
 
-    var bibtex_string = lines.join(',\n') + '\n}';
-    return bibtex_string;
-
-};
-
-var join_tokens = function (tokens) {
-    var out = [];
-    for (i = 0; i < tokens.length; i++) {
-        out.push(tokens[i]);
-        if (i < tokens.length-1) {
-            var tok1 = tokens[i+1];
-            if (!_.contains(['?', ')', ';', ':', ',', '.'], tok1)) {
-                out.push(' ')
+            if (tok[0] !== '(' && i < n - 1) {
+                if (!_.contains(['?', ')', ';', ':', ',', '.'], data.tokens[i + 1])) {
+                    o.append($('<span></span>').text(' '));
+                }
             }
         }
+
+        return o.children();
     }
-    return out.join('');
-};
 
-var build_plain_colorized = function (data) {
-    // o = $('#output-zone');
-    o = $('<div></div>');
-    o.text('');
-    var n = data['tokens'].length;
-    for (var i = 0; i < n; i++) {
-        var tok = data['tokens'][i];
-        var tag = data['tags'][i];
-        o.append($('<span></span>').text(tok).addClass(tag));
 
-        if (tok[0] == '(') {
-            continue;
+    /*
+        Build a BibTeX representation (string) of the tokenized/tagged
+        data returned from the API.
+    */
+    function build_bibtex(data) {
+        // Chunk the zipper into contiguous runs of the same tag.
+        var i,
+            last_tag = null,
+            chunk_toks = [],   // final length n_runs.
+            chunk_tags = [];   // final length n_runs.
+
+        var tok, tag;
+
+        // each chunk_toks[i] is a list of variable length, with the tokens
+        // in that run of consecutive tag.
+        for (i = 0; i < data.tokens.length; i += 1) {
+            tok = data.tokens[i];
+            tag = data.tags[i];
+
+            if (tag !== 'None') {
+                if (tag === last_tag) {
+                    chunk_toks[chunk_toks.length - 1].push(tok);
+                } else {
+                    chunk_toks.push([tok]);
+                    chunk_tags.push(tag);
+                }
+            }
+            last_tag = tag;
         }
 
-        if (i < n-1) {
-            tok1 = data['tokens'][i+1];
-            if (tok1 == ',' || tok1[0] == ')' || tok1 == ';' || tok1 == ':' ||
-                tok1 == '?' || tok1 == '.') {
-                continue;
+        var fields = {
+            authors: [{}],
+            title: [],
+            journal: [],
+            volume: [],
+            year: [],
+            issue: [],
+            page: [],
+        };
+
+        for (i = 0; i < chunk_toks.length; i += 1) {
+            if (chunk_tags[i] === 'journ') {
+                Array.prototype.push.apply(fields.journal, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'title') {
+                Array.prototype.push.apply(fields.title, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'vol') {
+                Array.prototype.push.apply(fields.volume, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'year') {
+                Array.prototype.push.apply(fields.year, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'page') {
+                Array.prototype.push.apply(fields.page, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'issue') {
+                Array.prototype.push.apply(fields.issue, chunk_toks[i]);
+            } else if (chunk_tags[i] === 'given' || chunk_tags[i] === 'fam') {
+                var authors = fields.authors;
+                if (Object.keys(authors[fields.authors.length - 1]).length === 2) {
+                    authors.push({});
+                }
+                var last_auth = authors[authors.length - 1];
+                last_auth[chunk_tags[i]] = chunk_toks[i];
+            }
+        }
+        console.log(chunk_tags);
+        console.log(chunk_toks);
+
+        // format the author list
+        var author_records = fields.authors,
+            authors = [];
+        for (i = 0; i < author_records.length; i += 1) {
+            var g = author_records[i].given;
+            if (g === undefined) {
+                g = [];
             }
 
-            o.append($('<span></span>').text(' '));
+            var f = author_records[i].fam;
+            if (f === undefined) {
+                f = [];
+            }
+            authors.push(join_tokens(g.concat(f)));
         }
+
+        var author_list = authors.join(' and '),
+            citekey = 'citekey';
+        if (author_records.length > 0) {
+            citekey = author_records[0].fam + join_tokens(fields.year);
+            if (fields.title.length > 0) {
+                citekey += fields.title[0];
+            }
+        }
+
+        var lines = ['@author{' + citekey];
+        if (author_list.length > 0) {
+            lines.push('  author = {' + author_list + '}');
+        }
+        if (fields.title.length > 0) {
+            lines.push('  title = {' + join_tokens(fields.title) + '}');
+        }
+        if (fields.journal.length > 0) {
+            lines.push('  journal = {' + join_tokens(fields.journal) + '}');
+        }
+        if (fields.year.length > 0) {
+            lines.push('  year = {' + join_tokens(fields.year) + '}');
+        }
+        if (fields.volume.length > 0) {
+            lines.push('  volume = {' + join_tokens(fields.volume) + '}');
+        }
+        if (fields.issue.length > 0) {
+            lines.push('  issue= {' + join_tokens(fields.issue) + '}');
+        }
+        if (fields.page.length > 0) {
+            lines.push('  page= {' + join_tokens(fields.page) + '}');
+        }
+
+        var bibtex_string = lines.join(',\n') + '\n}';
+        return bibtex_string;
+
     }
 
-    return o.children();
-};
+    /*
+        Utility function to join tokens with whitespace.
+    */
+    function join_tokens(tokens) {
+        var i,
+            out = [];
 
-
-$(function () {
-    if (window.location.hash == '#bibtex') {
-        $('#btn-toggle-format').text('BibTeX ').append('<span class="caret"></span>');
-    } else if (window.location.hash == '#bibtex') {
-        $('#btn-toggle-format').text('Plain ').append('<span class="caret"></span>');
+        for (i = 0; i < tokens.length; i += 1) {
+            out.push(tokens[i]);
+            if (i < tokens.length - 1) {
+                if (!_.contains(['?', ')', ';', ':', ',', '.'], tokens[i + 1])) {
+                    out.push(' ')
+                }
+            }
+        }
+        return out.join('');
     }
 
-    // placeholder
-    // $('#form-box').val('Baggiolini, M., Dewald, B. & Moser, B. Adv. Immunol. 55, 97-179 (1994)');
 
-    var queryTimeOut;
-    $("#form-box").keyup(function(){
-        var that = this;
-       // $(this).val($(this).val().replace(/\n/g, ' '));
-       // if (event.keyCode == 13) {
+    // main
+    $(function () {
+        if (window.location.hash == '#bibtex') {
+            $('#btn-toggle-format').text('BibTeX ').append('<span class="caret"></span>');
+        } else if (window.location.hash == '#plain') {
+            $('#btn-toggle-format').text('Plain ').append('<span class="caret"></span>');
+        }
 
-           clearTimeout(queryTimeOut);
-           setTimeout(function() {
-               query($(that).val());
-           }, 100);
-       // }
+        // placeholder
+        // $('#form-box').val('Springer, T.A. and James Carville  "Biological " J. Chem. Phys. 1993');
+
+        var queryTimeOut;
+        $("#form-box").keyup(function(){
+            var that = this;
+            clearTimeout(queryTimeOut);
+            setTimeout(function() {
+                query($(that).val());
+            }, 100);
+        });
+
+
+        $(".dropdown-menu li a").click(function() {
+            var top = $(this).parents(".btn-group").find('.btn');
+            top.text($(this).text() + ' ').append('<span class="caret"></span>');
+            render_output_zone();
+        });
     });
 
-
-    $(".dropdown-menu li a").click(function() {
-        var top = $(this).parents(".btn-group").find('.btn');
-        top.text($(this).text() + ' ').append('<span class="caret"></span>');
-        render_output_zone();
-    });
-});
-
-
-var SelectText = function (element) {
-    var doc = document
-        , text = doc.getElementById(element)
-        , range, selection
-    ;
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-};
+})();
