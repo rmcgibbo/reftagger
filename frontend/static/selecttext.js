99f180455f897df49ee453ee2f173d1fcff273db
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-18 22:26:01 -0800

    move one function to separate file

---
+++ frontend/static/selecttext.js
@@ -0,0 +1,19 @@
+/* Select an element (as if the user had selected a chunk of text w/ mouse)
+*/
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+};
