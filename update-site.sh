28f82272199df30f202dccb00eb4f6cab4b8e7c0
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-21 23:19:00 -0800

    requirements.txt

--- update-site.sh
+++ update-site.sh
@@ -2,6 +2,7 @@
 # and restart the supervisor
 ssh reftag.rmcgibbo.org \
     'cd /home/rmcgibbo/reftagger/  &&
+    /home/rmcgibbo/venv/bin/pip install -r requirements.txt &&
     git pull origin master &&
     cd /home/rmcgibbo/ &&
     supervisorctl restart tornado-5000'
