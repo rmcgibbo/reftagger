3c800536b0023cd68d5ee7f206d1e88ea4e488ac
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-21 18:49:04 -0800

    featurizer uses known journal matches

--- bibtagger/featurize.py
+++ bibtagger/featurize.py
@@ -4,18 +4,29 @@ import functools
 from pprint import pprint
 from os.path import join, dirname, abspath
 from pkg_resources import resource_filename
+
+import marisa_trie
 from nltk.corpus import wordnet as wn
+from bibtagger.tokenizer import untokenize, tokenize
+
+def _load_reference(fname, lower=False):
+    with gzip.open(resource_filename('bibtagger', join('fixeddata', fname))) as f:
+        items = set()
+        for line in f:
+            line = line.strip().decode('utf-8')
+            if lower:
+                line = line.lower()
+            items.add(line)
+        return frozenset(items)
+
 
 DIGITS = set([str(e) for e in range(10)])
 UPPERCASE = set(string.ascii_uppercase)
-REFERENCE_DATA = resource_filename('bibtagger', 'fixeddata')
-
-with gzip.open(join(REFERENCE_DATA, 'common-given-names.txt.gz')) as f:
-    COMMON_GIVEN_NAMES = frozenset({e.strip().decode('utf-8').lower() for e in f.readlines()})
-with gzip.open(join(REFERENCE_DATA,'common-surnames.txt.gz')) as f:
-    COMMON_SURNAMES = frozenset({e.strip().decode('utf-8').lower() for e in f.readlines()})
-with gzip.open(join(REFERENCE_DATA, 'common-words.txt.gz')) as f:
-    COMMON_WORDS = frozenset({e.strip().decode('utf-8').lower() for e in f.readlines()})
+COMMON_GIVEN_NAMES = _load_reference('common-given-names.txt.gz')
+COMMON_SURNAMES = _load_reference('common-surnames.txt.gz', lower=True)
+COMMON_WORDS = _load_reference('common-words.txt.gz')
+JOURNAL_SET = _load_reference('journals.txt.gz')
+JOURNAL_TRIE = marisa_trie.Trie(JOURNAL_SET)
 
 
 def common_hypernym(synsets):
@@ -59,9 +70,18 @@ def featurize(phrase):
             'all_num': all(l in DIGITS for l in word),
         }
 
+
     n = len(phrase)
     local_features = [get_local_features(word) for word in phrase]
     shift_features = [{} for _ in local_features]
+
+    for i in range(n):
+        local_features[i]['known_journal'] = False
+    for i in range(n):
+        matches = JOURNAL_TRIE.prefixes(untokenize(phrase[i:]))
+        for match in matches:
+            for j, tok in enumerate(tokenize(match)):
+                local_features[i+j]['known_journal'] = True
 
     for i in range(1, n):
         shift_features[i].update({k+'[-1]': v for k, v in local_features[i-1].items()})
@@ -78,5 +98,18 @@ def featurize(phrase):
     features[-1].append('__EOS__')
 
 
-
     return features
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    item = {"tagged": [
+        ["fam", "Massague"], ["given", "J"], [None, ","], ["fam", "Seoane"],
+        ["given", "J"], [None, ","], ["fam", "Wotton"], ["given", "D"],
+        [None, "("], ["year", "2005"], [None, ")"], ["title", "Smad"],
+        ["title", "transcription"], ["title", "factors"], [None, "."],
+        ["journ", "Genes"], ["journ", "Dev"], ["vol", "19"], [None, ":"],
+        ["page", "2783"], ["page", "-"], ["page", "2810"], [None, "."]]
+    }
+    features = featurize([e[1] for e in item['tagged']])
+    pprint(features[15])
