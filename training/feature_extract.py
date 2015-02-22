d0fce35058e096e2fe0b95f050c1e4e55a7fde67
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-21 19:10:41 -0800

    order

--- training/feature_extract.py
+++ training/feature_extract.py
@@ -11,7 +11,7 @@ import sys
 import json
 import pickle
 import argparse
-from os.path import dirname, abspath, join
+from os.path import dirname, abspath, join, isfile
 from collections import Counter
 
 PROJECT_ROOT = join(dirname(abspath(__file__)), '..')
@@ -22,20 +22,25 @@ from bibtagger.featurize import featurize
 def main():
     p = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('source', help='Tokenize and tagged citations (jsonlines format)')
+    p.add_argument('source', nargs='+', help='Tokenize and tagged citations (jsonlines format)')
     p.add_argument('dest', help='Featurized training data (pkl format)')
     p.add_argument('-n', '--n-rare', help='Minimum number of occurances of a '
         'token to label it \'rare\'.', type=int, default=10)
 
     args = p.parse_args()
+    if isfile(args.dest):
+        p.error('File exists. %s' % args.dest)
+
     phrases, y = [], []
 
-    with open(args.source, 'r') as f:
-        for i, line in enumerate(f):
-            item = json.loads(line)['tagged']
-            xx, yy = zip(*item)
-            phrases.append(xx)
-            y.append([str(tag) for tag in yy])
+    for source in args.source:
+        with open(source, 'r') as f:
+            for i, line in enumerate(f):
+                item = json.loads(line)['tagged']
+                if len(item) > 0:
+                    yy, xx = zip(*item)
+                    phrases.append(xx)
+                    y.append([str(tag) for tag in yy])
 
     print('Featurizing')
     X = []
@@ -46,7 +51,9 @@ def main():
 
     print('len(X)', len(X))
     print('len(y)', len(y))
+    print(X[0])
     print(y[0])
+
 
     with open(args.dest, 'wb') as fout:
         pickle.dump({
